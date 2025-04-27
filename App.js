9c3d8c55f8a3e4cb21febd8c08da2f5fadde06b1
Keith <krpbarber@gmail.com>
2025-04-26 17:22:55 -0700

    Added network error message

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform, View, StatusBar, StyleSheet } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+import { Platform, View, StatusBar, StyleSheet, Alert } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { registerRootComponent } from "expo";
+import Toast from "react-native-toast-message";
 import LazyImage from "./Components/LazyImage";
 import Planets from "./Components/Pages/Planets";
 import Films from "./Components/Pages/Films";
@@ -14,8 +16,52 @@ import styles from "./Components/Pages/styles";
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
+const connectedMap = {
+  none: "Disconnected",
+  unknown: "Disconnected",
+  wifi: "Connected",
+  cell: "Connected",
+  mobile: "Connected"
+};
+
+const networkErrorMessage = {
+  type: "error",
+  text1: "You're offline",
+  text2: "This app requires an internet connection.",
+  position: "bottom",
+  autoHide: false
+};
 
 export default function App() {
+  const  [connected, setConnected] = useState("Connected");
+  const previousNetworkState = useRef("Connected");
+
+  useEffect(() => {
+    function onNetworkChange(connection){
+      const networkState = connectedMap[connection.type];
+      if(networkState == "Disconnected" && 
+        previousNetworkState.current == "Connected")
+      {
+        Toast.show(networkErrorMessage);
+      }
+      else if(networkState == "Connected"){
+        Toast.hide();
+      }
+      previousNetworkState.current = networkState;
+      setConnected(networkState);
+    }
+    const unsubscribe = NetInfo.addEventListener(onNetworkChange);
+    return () =>  {
+      unsubscribe();
+    };
+  }, []);
+
+  function handleNavigationStateChange(){
+    if (previousNetworkState.current == "Disconnected"){
+      Toast.show(networkErrorMessage);
+    }
+  }
+
   function getHeaderImage(name){
     return{
       headerBackground: () => (
@@ -37,42 +83,47 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <StatusBar 
-        translucent={true} 
-        backgroundColor={"transparent"}
-        barStyle="light-content"
-      />
+    <>
+      <NavigationContainer
+        onStateChange={handleNavigationStateChange}
+      >
+        <StatusBar 
+          translucent={true} 
+          backgroundColor={"transparent"}
+          barStyle="light-content"
+        />
 
-      {Platform.OS === "ios" && (
-        <Tab.Navigator
-          screenOptions={{ 
-            headerTintColor: "white",
-            headerTitleAlign: "left"
-           }}
-        >
-          <Tab.Screen name="Planets" component={Planets} 
-            options={getHeaderImage("Planets")} />
-          <Tab.Screen name="Films" component={Films} 
-            options={getHeaderImage("Films")} />
-          <Tab.Screen name="Spaceships" component={Spaceships} 
-            options={getHeaderImage("Spaceships")} />
-        </Tab.Navigator>
-      )}
+        {Platform.OS === "ios" && (
+          <Tab.Navigator
+            screenOptions={{ 
+              headerTintColor: "white",
+              headerTitleAlign: "left"
+            }}
+          >
+            <Tab.Screen name="Planets" component={Planets} 
+              options={getHeaderImage("Planets")} />
+            <Tab.Screen name="Films" component={Films} 
+              options={getHeaderImage("Films")} />
+            <Tab.Screen name="Spaceships" component={Spaceships} 
+              options={getHeaderImage("Spaceships")} />
+          </Tab.Navigator>
+        )}
 
-      {Platform.OS == "android" && (
-        <Drawer.Navigator
-          screenOptions={{ headerTintColor: "white" }}
-        >
-          <Drawer.Screen name="Planets" component={Planets}
-            options={getHeaderImage("Planets")} />
-          <Drawer.Screen name="Films" component={Films} 
-            options={getHeaderImage("Films")} />
-          <Drawer.Screen name="Spaceships" component={Spaceships} 
-            options={getHeaderImage("Spaceships")} />
-        </Drawer.Navigator>
-      )}
-    </NavigationContainer>
+        {Platform.OS == "android" && (
+          <Drawer.Navigator
+            screenOptions={{ headerTintColor: "white" }}
+          >
+            <Drawer.Screen name="Planets" component={Planets}
+              options={getHeaderImage("Planets")} />
+            <Drawer.Screen name="Films" component={Films} 
+              options={getHeaderImage("Films")} />
+            <Drawer.Screen name="Spaceships" component={Spaceships} 
+              options={getHeaderImage("Spaceships")} />
+          </Drawer.Navigator>
+        )}
+      </NavigationContainer>
+      <Toast/>
+    </>
   );
 }
 
